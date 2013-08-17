cf68fdcaa67269d5cd047789970ea3a3dfcfba27
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2013-08-17 13:53:58 -0400

    made it public in amazon and using the right link now.
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,7 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   config.site_name = "Distributions Dady"
-  config.logo = "http://s3.amazonaws.com/dady/app/public/spree/eco tires protectant.png"
+  config.logo = "https://s3.amazonaws.com/dady/app/eco+tires+protectant.png"
 end
 
 Spree.user_class = "Spree::User"
